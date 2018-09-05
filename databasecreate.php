54e3629d2dd05122719efb1aed1be8dd0bf31702
Toby Dixon Smith <dixonsmith.ta@oundleschool.org.uk>
2018-09-05 12:14:49 +0100

    Created databasecreate.php and created SportsDB

---
+++ databasecreate.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+<body>
+  <?php
+  $servername = "localhost";
+  $username = "root";
+  $password = "";
+
+  // Create connection
+  $conn = new mysqli($servername, $username, $password);
+  // Check connection
+  if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+  }
+
+  // Create database
+  $sql = "CREATE DATABASE SportsDB";
+  if ($conn->query($sql) === TRUE) {
+      echo "Database created successfully";
+  } else {
+      echo "Error creating database: " . $conn->error;
+  }
+
+  $conn->close();
+  ?>
+</body>
+</head>
+</html>
