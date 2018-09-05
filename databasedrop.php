e3a8eea556611638631d4d9a97f980fe674ab4fd
Toby Dixon Smith <dixonsmith.ta@oundleschool.org.uk>
2018-09-05 12:29:42 +0100

    Created databasedrop.php in order to drop SportsDB

---
+++ databasedrop.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+<body>
+  <?php
+  $servername = "localhost";
+  $username = "root";
+  $password = "";
+
+  // Create connection
+  $conn = new mysqli($servername, $username, $password);
+  // Check connection
+  if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+  }
+
+  // Create database
+  $sql = "DROP DATABASE SportsDB";
+  if ($conn->query($sql) === TRUE) {
+      echo "Database dropped successfully";
+  } else {
+      echo "Error dropping database: " . $conn->error;
+  }
+
+  $conn->close();
+  ?>
+</body>
+</head>
+</html>
