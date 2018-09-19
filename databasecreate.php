a5b50586cac44e5915c541d292f99bc714ed7a59
Toby Dixon Smith <dixonsmith.ta@oundleschool.org.uk>
2018-09-19 10:02:31 +0100

    Create table sports

--- databasecreate.php
+++ databasecreate.php
@@ -18,12 +18,13 @@
   // Create database
   $sql = "CREATE DATABASE ".$dbname;
   if ($conn->query($sql) === TRUE) {
-      echo "Database created successfully";
+      echo "Database created successfully \n";
   } else {
       echo "Error creating database: " . $conn->error;
   }
 
   $conn->close();
+
     // Create connection
   $conn = mysqli_connect($servername, $username, $password, $dbname);
   // Check connection
@@ -33,15 +34,30 @@
 
   // sql to create table
   $sql = "CREATE TABLE sl (
-  address VARCHAR(20) PRIMARY KEY,
-  name VARCHAR(30) NOT NULL,
-  house CHAR(3) NOT NULL,
-  sex enum('M','F') DEFAULT 'M' NOT NULL,
-  year enum('1','2','3','4','5','6','7') DEFAULT '1' NOT NULL
+  Address VARCHAR(20) NOT NULL PRIMARY KEY UNIQUE,
+  Name VARCHAR(30) NOT NULL,
+  House CHAR(3) NOT NULL,
+  Sex enum('M','F') DEFAULT 'M' NOT NULL,
+  Year enum('1','2','3','4','5','6','7') DEFAULT '1' NOT NULL
   )";
 
   if ($conn->query($sql) === TRUE) {
-      echo "Table MyGuests created successfully";
+      echo "Table sl created successfully";
+  } else {
+      echo "Error creating table: " . $conn->error;
+  }
+
+  // sql to create table
+  $sql = "CREATE TABLE Sports (
+  Sport_ID INT(3) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  Name VARCHAR(30) NOT NULL,
+  HS_Name VARCHAR(30) NOT NULL,
+  HS_Address VARCHAR(20) NOT NULL,
+  Current enum('0','1') DEFAULT '1' NOT NULL
+  )";
+
+  if ($conn->query($sql) === TRUE) {
+      echo "Table Sports created successfully";
   } else {
       echo "Error creating table: " . $conn->error;
   }
