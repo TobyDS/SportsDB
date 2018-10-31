8188c373ab73f7806e86937d45f396c555f9b90a
Toby Dixon Smith <dixonsmith.ta@oundleschool.org.uk>
2018-10-31 12:40:28 +0000

    Reads in sports and choices

--- filldatabase.php
+++ filldatabase.php
@@ -60,6 +60,32 @@ if($_SERVER['REQUEST_METHOD'] == "POST" and isset($_POST['someAction']))
         } else {
             echo "Problem in Importing CSV Data";
         }
+      // Reads sports.csv into Sports table
+      $file = fopen("csvs/sports.csv", "r");
+      while (($column = fgetcsv($file, 10000, ",")) !== FALSE) {
+          $sqlInsert = "INSERT into Sports (Name)
+                  values ('".$column[0]."')";
+          $result = mysqli_query($conn, $sqlInsert);
+        }
+
+        if (! empty($result)) {
+            echo nl2br("Sports CSV Data Imported into the Database \n");
+        } else {
+            echo "Problem in Importing CSV Data";
+        }
+      // Reads choices.csv into Choices table
+      $file = fopen("csvs/choices.csv", "r");
+      while (($column = fgetcsv($file, 10000, ",")) !== FALSE) {
+          $sqlInsert = "INSERT into Choices (Sex,Term_ID,Year_ID,Sport_ID)
+                  values ('" . $column[0] . "','" . $column[1] . "','" . $column[2] . "','" . $column[3] . "')";
+          $result = mysqli_query($conn, $sqlInsert);
+        }
+
+        if (! empty($result)) {
+            echo nl2br("Choices CSV Data Imported into the Database \n");
+        } else {
+            echo "Problem in Importing CSV Data";
+        }
   }
 ?>
 <html>
