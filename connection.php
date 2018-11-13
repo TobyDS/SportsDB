d7c5affeb6d8a9ebd6efcf018d42ea679749af75
Toby Dixon Smith <dixonsmith.ta@oundleschool.org.uk>
2018-11-13 20:16:13 +0000

    Port to 3306

--- connection.php
+++ connection.php
@@ -3,9 +3,8 @@ $servername = "fdb23.awardspace.net";
 $username = "2880778_sportsdb";
 $password = "L@nhams1";
 $dbname = "2880778_sportsdb";
-
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn = new PDO("mysql:host=$servername;port=3306;dbname=$dbname", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
